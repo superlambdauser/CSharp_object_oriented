1e6eda582498fea08bddb70cbc2286ab312f5697
superlambdauser <r.froment@interface3.be>
2025-06-16 16:02:18 +0200

    Abstract kw

---
+++ Demo_Abstract/Models/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Abstract.Models
+{
+    internal abstract class Animal
+    {
+        public string Name { get; private set; }
+
+        protected Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Scream();
+    }
+}
---
+++ Demo_Abstract/Models/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Abstract.Models
+{
+    internal sealed class Dog : Animal
+    {
+        public Dog(string name) : base(name)
+        {
+
+        }
+
+        public override void Scream()
+        {
+            Console.WriteLine("Woof.");
+        }
+    }
+}
